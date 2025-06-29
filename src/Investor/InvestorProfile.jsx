f2e8df469487359497a3d7e198c9fb52b8249ff4
Pawan Maurya <pawanmaurya8931@gmail.com>
2025-06-29 16:39:43 +0530

    Css a add in InvestorProfile.jsx

--- src/Investor/InvestorProfile.jsx
+++ src/Investor/InvestorProfile.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import axios from "axios";
-import "../assets/css/investorProfile.css";
+import "../assets/css/investorProfile";
 import admin from '../assets/img/admin/admin.png'
 
 const InvestorProfile = () => {
